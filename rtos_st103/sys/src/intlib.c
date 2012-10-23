0972fe736223b9c005281914c718e07061230167
liuning <liuning@a4f59d23-0ed6-be48-878f-d394f869a453>
2012-10-23 14:46:24 +0000

    修改中断个数59。
    git-svn-id: https://127.0.0.1:5678/svn/st103/trunk@25 a4f59d23-0ed6-be48-878f-d394f869a453

--- rtos_st103/sys/src/intlib.c
+++ rtos_st103/sys/src/intlib.c
@@ -110,53 +110,6 @@ void	routine57	(void)	{ intHandler(	57	); }
 void	routine58	(void)	{ intHandler(	58	); }
 void	routine59	(void)	{ intHandler(	59	); }
 
-void	routine60	(void)	{ intHandler(	60	); }
-void	routine61	(void)	{ intHandler(	61	); }
-void	routine62	(void)	{ intHandler(	62	); }
-void	routine63	(void)	{ intHandler(	63	); }
-void	routine64	(void)	{ intHandler(	64	); }
-void	routine65	(void)	{ intHandler(	65	); }
-void	routine66	(void)	{ intHandler(	66	); }
-void	routine67	(void)	{ intHandler(	67	); }
-void	routine68	(void)	{ intHandler(	68	); }
-void	routine69	(void)	{ intHandler(	69	); }
-void	routine70	(void)	{ intHandler(	70	); }
-void	routine71	(void)	{ intHandler(	71	); }
-void	routine72	(void)	{ intHandler(	72	); }
-void	routine73	(void)	{ intHandler(	73	); }
-void	routine74	(void)	{ intHandler(	74	); }
-void	routine75	(void)	{ intHandler(	75	); }
-void	routine76	(void)	{ intHandler(	76	); }
-void	routine77	(void)	{ intHandler(	77	); }
-void	routine78	(void)	{ intHandler(	78	); }
-void	routine79	(void)	{ intHandler(	79	); }
-void	routine80	(void)	{ intHandler(	80	); }
-void	routine81	(void)	{ intHandler(	81	); }
-void	routine82	(void)	{ intHandler(	82	); }
-void	routine83	(void)	{ intHandler(	83	); }
-void	routine84	(void)	{ intHandler(	84	); }
-void	routine85	(void)	{ intHandler(	85	); }
-void	routine86	(void)	{ intHandler(	86	); }
-void	routine87	(void)	{ intHandler(	87	); }
-void	routine88	(void)	{ intHandler(	88	); }
-
-void	routine89	(void)	{ intHandler(	89	); }
-void	routine90	(void)	{ intHandler(	90	); }
-void	routine91	(void)	{ intHandler(	91	); }
-void	routine92	(void)	{ intHandler(	92	); }
-void	routine93	(void)	{ intHandler(	93	); }
-void	routine94	(void)	{ intHandler(	94	); }
-void	routine95	(void)	{ intHandler(	95	); }
-void	routine96	(void)	{ intHandler(	96	); }
-void	routine97	(void)	{ intHandler(	97	); }
-void	routine98	(void)	{ intHandler(	98	); }
-void	routine99	(void)	{ intHandler(	99	); }
-void	routine100	(void)	{ intHandler(	100	); }
-void	routine101  (void)	{ intHandler(	101	); }
-void	routine102  (void)	{ intHandler(	102	); }
-void	routine103	(void)	{ intHandler(	103	); }
-
-
 
 extern unsigned long cstack_top; /* Defined by the linker */
 
@@ -230,51 +183,6 @@ const INTVECT_ITEM __vector_table[] = {
     ROUTINE(57 ),
     ROUTINE(58 ),
     ROUTINE(59 ),
-
-    ROUTINE(60 ),
-    ROUTINE(61 ),
-    ROUTINE(62 ),
-    ROUTINE(63 ),
-    ROUTINE(64 ),
-    ROUTINE(65 ),
-    ROUTINE(66 ),
-    ROUTINE(67 ),
-    ROUTINE(68 ),
-    ROUTINE(69 ),
-    ROUTINE(70 ),
-    ROUTINE(71 ),
-    ROUTINE(72 ),
-    ROUTINE(73 ),
-    ROUTINE(74 ),
-    ROUTINE(75 ),
-    ROUTINE(76 ),
-    ROUTINE(77 ),
-    ROUTINE(78 ),
-    ROUTINE(79 ),
-    ROUTINE(80 ),
-    ROUTINE(81 ),
-    ROUTINE(82 ),
-    ROUTINE(83 ),
-    ROUTINE(84 ),
-    ROUTINE(85 ),
-    ROUTINE(86 ),
-    ROUTINE(87 ),
-    ROUTINE(88 ),
-    ROUTINE(89 ),
-    ROUTINE(90 ),
-    ROUTINE(91 ),
-    ROUTINE(92 ),
-    ROUTINE(93 ),
-    ROUTINE(94 ),
-    ROUTINE(95 ),
-    ROUTINE(96 ),
-    ROUTINE(97 ),
-    ROUTINE(98 ),
-    ROUTINE(99 ),
-    ROUTINE(100),
-    ROUTINE(101),
-    ROUTINE(102),
-    ROUTINE(103)
 };
 
 /*-----------------------------------------------------------------------------
