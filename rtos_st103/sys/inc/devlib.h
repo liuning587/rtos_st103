ba5a52ead694656f6213f735ab94aa50c2b46149
liuning <liuning@a4f59d23-0ed6-be48-878f-d394f869a453>
2012-11-14 15:24:39 +0000

    提交devlib模块  未测试。
    git-svn-id: https://127.0.0.1:5678/svn/st103/trunk@37 a4f59d23-0ed6-be48-878f-d394f869a453

--- rtos_st103/sys/inc/devlib.h
+++ rtos_st103/sys/inc/devlib.h
@@ -12,21 +12,40 @@
 #define __DEVLIB_H__
 
 #include <types.h>
-#if 0
+
 #define MAX_DEVICE_NAME     (5u)    /**< 最大设备名长度 */
 #define MAX_DEVICE_NUM      (5u)    /**< 最大设备数 */
+#define MAX_OPEN_NUM        (5u)    /**< 最大同时打开设备数 */
 
-typedef struct
+
+typedef struct fileopt
 {
-    status_t  (*init)   (device_t dev);
-    status_t  (*open)   (device_t dev, uint16_t oflag);
-    status_t  (*close)  (device_t dev);
-    size_t    (*read)   (device_t dev, off_t pos, void *buffer, size_t size);
-    size_t    (*write)  (device_t dev, off_t pos, const void *buffer, size_t size);
-    status_t  (*ioctl)  (device_t dev, uint8_t cmd, void *args);
+    status_t  (*init)   (struct fileopt* dev);
+    status_t  (*release)(struct fileopt* dev);
+    int32_t   (*open)   (struct fileopt* dev, uint16_t oflag);
+    status_t  (*close)  (struct fileopt* dev);
+    size_t    (*read)   (struct fileopt* dev, long pos, void *buffer, size_t size);
+    size_t    (*write)  (struct fileopt* dev, long pos, const void *buffer, size_t size);
+    int32_t   (*ioctl)  (struct fileopt* dev, uint32_t cmd, void *args);
+    status_t   (*lock)   (struct fileopt* dev);   /**< 读写锁 */
+    status_t   (*unlock) (struct fileopt* dev);   /**< 读写锁 */
 } fileopt_t;
 
-#endif
+typedef struct device
+{
+    struct device*  next;
+    char_t name[MAX_DEVICE_NAME];   /**< 设备名 */
+    int32_t offset;                 /**< 读写偏移地址 */
+    struct fileopt fileopt;         /**< 设备操作方法 */
+} device_t;
 
+extern status_t devlib_init(void);
+extern status_t dev_create(const char_t* pname, fileopt_t* pfileopt);
+extern status_t dev_release(const char_t* pname);
+extern int32_t dev_open(const char_t* pname, int32_t flags);
+extern int32_t dev_read(int32_t fd, void* buf, int32_t count);
+extern int32_t dev_write(int32_t fd, const void* buf, int32_t count);
+extern int32_t dev_ioctl(int32_t fd, uint32_t cmd, void *args);
+extern int32_t dev_close(int32_t fd);
 
 #endif /* __DEVLIB_H__ */
--- rtos_st103/sys/src/devlib.c
+++ rtos_st103/sys/src/devlib.c
@@ -2,20 +2,50 @@
  ******************************************************************************
  * @file       devlib.c
  * @version    V1.1.4
- * @brief      设备驱动操作
+ * @brief      设备驱动操作 todo:节点操作进入临界区
  * @details    This file including all API functions's implement of dps.
  * @copy       Copyrigth(C)
  *
  ******************************************************************************
  */
+#include <types.h>
+#include <stdio.h>
+#include <string.h>
 #include <devlib.h>
-#if 0
-typedef struct
+
+static device_t* pfree = NULL;
+static device_t* phead = NULL;
+static device_t the_devs[MAX_DEVICE_NUM];
+static device_t* the_opend_devs[MAX_OPEN_NUM];
+
+/**
+ ******************************************************************************
+ * @brief 寻找空闲句柄空间
+ * @param[in]  None
+ * @param[out] None
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+static int32_t
+find_free_fd(device_t* pnode)
 {
-    device_t*  next;
-    int8_t name[MAX_DEVICE_NAME];   /**< 设备名 */
-    fileopt_t fileopt;              /**< 设备操作方法 */
-} device_t;
+    for (int32_t i = 0; i < MAX_OPEN_NUM; i++)
+    {
+        if (the_opend_devs[i] == NULL)
+        {
+            the_opend_devs[i] = pnode;
+            Dprintf("find free fd node\n");
+            return i + 1;
+        }
+    }
+    Dprintf("can not find free fd node\n");
+
+    return -1;
+}
 
 /**
  ******************************************************************************
@@ -29,10 +59,104 @@ typedef struct
  * @note
  ******************************************************************************
  */
+status_t
+devlib_init(void)
+{
+    uint32_t i;
+
+    memset(the_opend_devs, 0x00, sizeof(the_opend_devs));
+    memset(the_devs, 0x00, sizeof(the_devs));
+    for (i = 0; i < MAX_DEVICE_NUM - 1; i++)
+    {
+        the_devs[i].next = &the_devs[i + 1];
+    }
+    the_devs[i].next = NULL;
+
+    pfree = &the_devs[0];
+    phead = &the_devs[0];
+
+    Dprintf("init OK\n");
+
+    return OK;
+}
 
 /**
  ******************************************************************************
- * @brief
+ * @brief 寻找已注册的设备 (按名称查找)
+ * @param[in]  None
+ * @param[out] None
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+static device_t*
+find_dev_by_name(const char_t* pname)
+{
+    device_t* pnode = phead;
+    while (pnode != NULL)
+    {
+        if (strncpy(pnode->name, pname, sizeof(pnode->name)) == 0)
+        {
+            return pnode;
+        }
+        pnode++;
+    }
+    return NULL;
+}
+
+/**
+ ******************************************************************************
+ * @brief 创建设备
+ * @param[in]  *pname
+ * @param[in]  *pfileopt
+ * @param[out] None
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+status_t
+dev_create(const char_t* pname, fileopt_t* pfileopt)
+{
+    device_t* new = NULL;
+
+    if ((pname == NULL) || (pfileopt == NULL) || (pfree == NULL))
+    {
+        return ERROR;
+    }
+
+    /* 防止重复注册 */
+    if (find_dev_by_name(pname) != NULL)
+    {
+        return ERROR;
+    }
+    new = pfree;
+    pfree = new->next;
+
+    strncpy(new->name, pname, sizeof(new->name));
+    memcpy(new, pfileopt, sizeof(fileopt_t));
+    if (new->fileopt.init == NULL)
+    {
+        if (new->fileopt.init(&new->fileopt) != OK)
+        {
+            /* 若执行初始化方法失败,则释放节点 */
+            pfree = new;
+            new = NULL;
+            return ERROR;
+        }
+    }
+
+    return OK;
+}
+
+/**
+ ******************************************************************************
+ * @brief 设备释放
  * @param[in]  None
  * @param[out] None
  * @retval     None
@@ -43,11 +167,255 @@ typedef struct
  ******************************************************************************
  */
 status_t
-dev_create(int8_t* pdevname, fileopt_t* pfileopt)
+dev_release(const char_t* pname)
 {
-    if ((pdevname == NULL) || (pfileopt == NULL))
+    device_t* pnode = phead;
+
+    if ((pname == NULL) || (phead == NULL))
     {
-        return ERROR;
+        return -1;
     }
+
+    /* 判断设备是否在使用 */
+    for (int32_t i = 0; i < MAX_OPEN_NUM; i++)
+    {
+        if (strncpy(the_opend_devs[i]->name, pname, sizeof(the_opend_devs[i]->name)) == 0)
+        {
+            Dprintf("dev:%s is using\n", pname);
+            return ERROR;
+        }
+    }
+
+    /* 首先判断第一个节点 */
+    if (strncmp(pnode->name, pname, sizeof(pnode->name)) == 0)
+    {
+        phead = phead->next;
+        pnode->next = pfree;
+        pfree->next = pnode;
+        return pnode->fileopt.release(&pnode->fileopt);
+    }
+
+    device_t* pnode_last = pnode;
+    pnode = pnode->next;
+    while (pnode != NULL)
+    {
+        if (strncmp(pnode->name, pname, sizeof(pnode->name)) == 0)
+        {
+            /* 找到对应节点 */
+            pnode_last->next = pnode->next;
+            pnode->next = pfree;
+            pfree->next = pnode;
+            return pnode->fileopt.release(&pnode->fileopt);
+        }
+        pnode_last = pnode;
+        pnode++;
+    }
+    Dprintf("can not find name:%s", pname);
+
+    return ERROR;
 }
-#endif
+
+/**
+ ******************************************************************************
+ * @brief 设备打开
+ * @param[in]  *pname   : 设备名
+ * @param[in]   flags   : O_RDONLY | OWRONLY | O_RDWR
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+int32_t
+dev_open(const char_t* pname, int32_t flags)
+{
+    device_t* pnode = pfree;
+
+    if (pname == NULL)
+    {
+        return -1;
+    }
+
+    while (pnode != NULL)
+    {
+        if (strncmp(pnode->name, pname, sizeof(pnode->name)) == 0)
+        {
+            /* 找到对应节点 */
+            return find_free_fd(pnode);
+        }
+        pnode++;    /* next node */
+    }
+    return -1;  /* can not find pname */
+}
+
+/**
+ ******************************************************************************
+ * @brief 判断句柄是否合法
+ * @param[in]  None
+ * @param[out] None
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+static bool_e
+is_fd_valid(int32_t fd)
+{
+    if (fd - 1 >= MAX_OPEN_NUM)
+    {
+        Dprintf("fd is out of range!\n");
+        return FALSE;
+    }
+
+    if (the_opend_devs[fd] == NULL)
+    {
+        Dprintf("fd is not opend!\n");
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+/**
+ ******************************************************************************
+ * @brief 设备读
+ * @param[in]  None
+ * @param[out] None
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+int32_t
+dev_read(int32_t fd, void* buf, int32_t count)
+{
+    int32_t size = -1;
+
+    if (FALSE == is_fd_valid(fd))
+    {
+        return -1;
+    }
+
+    if (the_opend_devs[fd]->fileopt.lock != NULL)
+    {
+        if (OK != the_opend_devs[fd]->fileopt.lock(&the_opend_devs[fd]->fileopt))
+        {
+            return -1;
+        }
+
+    }
+    size = the_opend_devs[fd]->fileopt.read(&the_opend_devs[fd]->fileopt,
+            the_opend_devs[fd]->offset,
+            buf,
+            count);
+    if (the_opend_devs[fd]->fileopt.unlock != NULL)
+    {
+        if (OK != the_opend_devs[fd]->fileopt.unlock(&the_opend_devs[fd]->fileopt))
+        {
+            return -1;
+        }
+    }
+    return size;
+}
+
+/**
+ ******************************************************************************
+ * @brief 设备写
+ * @param[in]  None
+ * @param[out] None
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+int32_t
+dev_write(int32_t fd, const void* buf, int32_t count)
+{
+    int32_t size = -1;
+
+    if (FALSE == is_fd_valid(fd))
+    {
+        return -1;
+    }
+
+    if (the_opend_devs[fd]->fileopt.lock != NULL)
+    {
+        if (OK != the_opend_devs[fd]->fileopt.lock(&the_opend_devs[fd]->fileopt))
+        {
+            return -1;
+        }
+
+    }
+    size = the_opend_devs[fd]->fileopt.write(&the_opend_devs[fd]->fileopt,
+            the_opend_devs[fd]->offset,
+            buf,
+            count);
+    if (the_opend_devs[fd]->fileopt.unlock != NULL)
+    {
+        if (OK != the_opend_devs[fd]->fileopt.unlock(&the_opend_devs[fd]->fileopt))
+        {
+            return -1;
+        }
+    }
+    return size;
+}
+
+/**
+ ******************************************************************************
+ * @brief 设备ioctl
+ * @param[in]  None
+ * @param[out] None
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+int32_t
+dev_ioctl(int32_t fd, uint32_t cmd, void *args)
+{
+    if (FALSE == is_fd_valid(fd))
+    {
+        return -1;
+    }
+    //ioctl 不上锁吗？
+    return the_opend_devs[fd]->fileopt.ioctl(&the_opend_devs[fd]->fileopt,
+            cmd,
+            args);
+}
+
+/**
+ ******************************************************************************
+ * @brief 设备关闭
+ * @param[in]  None
+ * @param[out] None
+ * @retval     None
+ *
+ * @details
+ *
+ * @note
+ ******************************************************************************
+ */
+int32_t
+dev_close(int32_t fd)
+{
+    if (FALSE == is_fd_valid(fd))
+    {
+        return -1;
+    }
+    if (OK != the_opend_devs[fd]->fileopt.close(&the_opend_devs[fd]->fileopt))
+    {
+        return -1;
+    }
+    the_opend_devs[fd] = NULL;  /* release fd */
+
+    return 1;
+}
